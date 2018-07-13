625721ad290a02ffaeccb875134aa64a5087e422
ajie47 <ajiedwi4798@gmail.com>
2018-07-13 09:56:59 +0700

    Tambah Model

---
+++ app/src/main/java/com/kode/kreasi/kodekreasirecruitment/Model/Model.java
@@ -0,0 +1,62 @@
+package com.kode.kreasi.kodekreasirecruitment.Model;
+
+public class Model {
+
+    String id, nama, alamat, provinsi, kota, tanggal_lahir, no_hp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getTanggal_lahir() {
+        return tanggal_lahir;
+    }
+
+    public void setTanggal_lahir(String tanggal_lahir) {
+        this.tanggal_lahir = tanggal_lahir;
+    }
+
+    public String getNo_hp() {
+        return no_hp;
+    }
+
+    public void setNo_hp(String no_hp) {
+        this.no_hp = no_hp;
+    }
+}
